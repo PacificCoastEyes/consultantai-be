00581456a59504a2d562345c9907169993e02119
Amitai Zand <amitai17@gmail.com>
2023-08-06 21:51:33 +0300

    Completed signup and login routes with JWT and bcrypt

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
+import os, datetime
 import asyncio
 from operator import itemgetter
 from flask import Flask, request, Response, Blueprint
 from flask_cors import CORS, cross_origin
 from prisma import Prisma
+from dotenv import load_dotenv, find_dotenv
+import bcrypt
+import jwt
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -11,14 +15,64 @@ loop = asyncio.get_event_loop()
 
 db = Prisma()
 
+load_dotenv(find_dotenv())
+
+
+def sign_token(user):
+    return jwt.encode(
+        {
+            "id": user.id,
+            "name": user.name,
+            "isAdmin": user.is_admin,
+            "iat": datetime.datetime.now(),
+            "exp": datetime.datetime.now() + datetime.timedelta(days=7),
+        },
+        os.environ.get("TOKEN_SECRET"),
+    )
+
+
+async def login_async(email, password):
+    await db.connect()
+    user = await db.user.find_first(where={"email": email})
+    await db.disconnect()
+    if user:
+        passwords_match = bcrypt.checkpw(
+            password.encode("utf-8"), user.password.encode("utf-8")
+        )
+        if passwords_match:
+            auth_token = sign_token(user)
+            return {
+                "authToken": auth_token,
+                "firstName": user.name.split(" ")[0],
+                "isAdmin": user.is_admin,
+            }
+        else:
+            return None
+    else:
+        return None
+
 
 async def register_async(name, email, password):
     await db.connect()
+    encrypted_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
     new_user = await db.user.create(
-        {"name": name, "email": email, "password": password, "is_admin": False}
+        {
+            "name": name,
+            "email": email,
+            "password": encrypted_password.decode("utf-8"),
+            "is_admin": False,
+        }
     )
     await db.disconnect()
-    return new_user.id
+    if new_user:
+        auth_token = sign_token(new_user)
+        return {
+            "authToken": auth_token,
+            "firstName": new_user.name.split(" ")[0],
+            "isAdmin": new_user.is_admin,
+        }
+    else:
+        return None
 
 
 async def check_if_existing_user_async(email):
@@ -56,12 +110,34 @@ def check_if_existing_user():
         )
 
 
+@bp.route("/login", methods=["POST"])
+@cross_origin()
+def login():
+    email, password = itemgetter("email", "password")(request.json)
+    try:
+        login_payload = loop.run_until_complete(login_async(email, password))
+        if login_payload:
+            return login_payload
+        else:
+            raise Exception("Invalid credentials")
+    except Exception as e:
+        print(e)
+        return Response(
+            "The email address and password you entered do not match",
+            status=400,
+        )
+
+
 @bp.route("/register", methods=["POST"])
 @cross_origin()
 def register():
     name, email, password = itemgetter("name", "email", "password")(request.json)
     try:
-        return loop.run_until_complete(register_async(name, email, password))
+        signup_payload = loop.run_until_complete(register_async(name, email, password))
+        if signup_payload:
+            return signup_payload
+        else:
+            raise Exception("Error adding new user to database.")
     except Exception as e:
         print(e)
         return Response(
