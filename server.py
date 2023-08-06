800a2b3e0e036d624ce91e3cf094b17b5dcbb235
Amitai Zand <amitai17@gmail.com>
2023-08-06 10:04:51 +0300

    Added 'api' URL prefix for all routes via blueprint

--- server.py
+++ server.py
@@ -1,33 +1,41 @@
 import asyncio
 from operator import itemgetter
-from flask import Flask, request, Response
+from flask import Flask, request, Response, Blueprint
 from flask_cors import CORS, cross_origin
 from prisma import Prisma
 
 app = Flask(__name__)
 cors = CORS(app)
 
+loop = asyncio.get_event_loop()
+
 db = Prisma()
 
-loop = asyncio.get_event_loop()
 
 async def register_async(name, email, password):
     await db.connect()
-    new_user = await db.user.create({
-        "name": name,
-        "email": email,
-        "password": password,
-        "is_admin": False
-    })
+    new_user = await db.user.create(
+        {"name": name, "email": email, "password": password, "is_admin": False}
+    )
     await db.disconnect()
     return new_user.id
 
-@app.route("/register", methods=["POST"])
+
+bp = Blueprint("api", __name__, url_prefix="/api")
+
+
+@bp.route("/register", methods=["POST"])
 @cross_origin()
 def register():
     name, email, password = itemgetter("name", "email", "password")(request.json)
-    try: 
+    try:
         return loop.run_until_complete(register_async(name, email, password))
     except Exception as e:
         print(e)
-        return Response("Sorry, there was a problem signing you up. Please try again later.", status=400)
+        return Response(
+            "Sorry, there was a problem signing you up. Please try again later.",
+            status=400,
+        )
+
+
+app.register_blueprint(bp)
